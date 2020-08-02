ab240ee5170b39f72df26f952969069549deb176
kevinserrano <66225834+kevinserrano@users.noreply.github.com>
2020-08-02 14:38:53 -0500

    added index with css and js

---
+++ script.js
@@ -0,0 +1,17 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", writePassword);
+// Write password to the #password input
+function writePassword() {
+    var password = generatePassword();
+
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+
+}
+
+function generatePassword() {
+    return "kevin";
+
+}
